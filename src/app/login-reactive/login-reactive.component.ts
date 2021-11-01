5ce6557b04955620935a948d18009ee0771b3fa5
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2021-11-01 00:15:07 -0600

    Add validation for reactive forms

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {createPasswordStrengthValidator} from '../validators/password-strength.validator';
 
 
@@ -11,27 +11,26 @@ import {createPasswordStrengthValidator} from '../validators/password-strength.v
 })
 export class LoginReactiveComponent implements OnInit {
 
-  email = new FormControl('', {
-    validators: [Validators.required, Validators.email],
-    updateOn: 'blur'
-  });
-  password = new FormControl('', {
-    validators: [Validators.required, Validators.minLength(8), createPasswordStrengthValidator()],
-    updateOn: 'blur'
-  });
-
-  form = new FormGroup({
-    email: this.email,
-    password: this.password,
+  form = this.fb.group({
+    email: ['', {
+      validators: [Validators.required, Validators.email],
+      updateOn: 'blur'
+    }],
+    password: ['', [Validators.required, Validators.minLength(8), createPasswordStrengthValidator()]],
   });
 
-  constructor() {
-
-
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit() {
+  }
 
+  get email() {
+    return this.form.controls['email'];
+  }
+
+  get password() {
+    return this.form.controls['email'];
   }
 
 }
