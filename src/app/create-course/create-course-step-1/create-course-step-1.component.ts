e3f7bed333ae20dbd77a9683aae09ecfcda2e1ac
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2021-11-04 14:50:08 -0600

    Add API validatior for title

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -3,16 +3,38 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CoursesService} from '../../services/courses.service';
 import {Observable} from 'rxjs';
 import {filter} from 'rxjs/operators';
+import {CourseTitleValidator} from '../../validators/course-title.validator';
+import {Course} from '../../model/course';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'create-course-step-1',
   templateUrl: './create-course-step-1.component.html',
   styleUrls: ['./create-course-step-1.component.scss']
 })
 export class CreateCourseStep1Component implements OnInit {
 
+  form = this.fb.group({
+    title: ['', {
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60)
+      ],
+      asyncValidators: [CourseTitleValidator(this.courses)],
+      updateOn: 'blur'
+    }],
+  });
+
+  constructor(private fb: FormBuilder, private courses: CoursesService) {
+  }
+
   ngOnInit() {
 
+  }
+
+  get courseTitle() {
+    return this.form.controls['title'];
   }
 
 }
