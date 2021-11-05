440d0fe6457009a570dfd40f1e60bf292c23b813
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2021-11-04 23:00:07 -0600

    Add Control Access validator to address

--- src/app/address-form/address-form.component.ts
+++ src/app/address-form/address-form.component.ts
@@ -11,25 +11,64 @@ import {
 import {noop, Subscription} from 'rxjs';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'address-form',
   templateUrl: './address-form.component.html',
-  styleUrls: ['./address-form.component.scss']
+  styleUrls: ['./address-form.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    multi: true,
+    useExisting: AddressFormComponent
+  }]
 })
-export class AddressFormComponent {
+export class AddressFormComponent implements ControlValueAccessor, OnDestroy {
 
-    @Input()
-    legend:string;
+  constructor(private fb: FormBuilder) {
+  }
 
-    form: FormGroup = this.fb.group({
-        addressLine1: [null, [Validators.required]],
-        addressLine2: [null, [Validators.required]],
-        zipCode: [null, [Validators.required]],
-        city: [null, [Validators.required]]
-    });
+  @Input()
+  legend: string;
+  onChangeSub: Subscription;
 
-    constructor(private fb: FormBuilder) {
+  form: FormGroup = this.fb.group({
+    addressLine1: [null, [Validators.required]],
+    addressLine2: [null, [Validators.required]],
+    zipCode: [null, [Validators.required]],
+    city: [null, [Validators.required]]
+  });
+
+  onTouched = () => {};
+
+  // its used from the parent component to write new values on the child component
+  writeValue(value: any): void {
+    if (value) {
+      this.form.setValue(value);
     }
+  }
 
+  // Use on the parent form to register the touched component
+  // User interacted with the form
+  registerOnTouched(onTouched: any): void {
+    this.onTouched = onTouched;
+  }
+
+  // If the control should be enable/disable
+  setDisabledState(disabled: boolean) {
+    if (disabled) {
+      this.form.disable();
+    } else {
+      this.form.enable();
+    }
+  }
+
+  // When the form values changes report to the parent component
+  registerOnChange(onChange: any): void {
+    this.onChangeSub = this.form.valueChanges.subscribe(onChange);
+  }
+
+  ngOnDestroy(): void {
+    this.onChangeSub.unsubscribe();
+  }
 }
 
 
